86be4a389784aec89a7e762ba51f787cc381c8b7
asto18089 <asto18089@126.com>
2023-03-10 18:30:54 +0800

    Better English

--- src/main.cpp
+++ src/main.cpp
@@ -95,12 +95,12 @@ int main()
 
     for (const auto &i : cpu_controller.get_super_table())
         log.write(LogLevel::Info, ("Virtual Freq: " + std::to_string(i)).c_str());
-    log.write(LogLevel::Info, "The virtual frequency table was created successfully");
+    log.write(LogLevel::Info, "Successfully created virtual frequency table");
 
     cpu_controller.set_scaling(2);
 
     start_close_others();
-    log.write(LogLevel::Info, "The cleanup process starts");
+    log.write(LogLevel::Info, "Starting cleanup process");
 
     while (true)
     {
@@ -116,7 +116,7 @@ int main()
         const jank_data jdata = analyzeFrameData(getOriginalData());
         if (jdata.empty())
         {
-            log.write(LogLevel::Debug, "Empty jank data !");
+            log.write(LogLevel::Debug, "Empty jank data!");
             continue;
         }
 
@@ -127,28 +127,28 @@ int main()
         constexpr double left = 0.01;
         constexpr double right = 0.02;
         if (nice >= left && nice <= right)
-            log.write(LogLevel::Debug, "The proportion of frame delay is in line with expectations");
+            log.write(LogLevel::Debug, "Proportion of frame delay is in line with expectation");
         else if (nice < left) // 掉帧少了，有余量
         {
             cpu_controller.limit(-1);
-            log.write(LogLevel::Debug, "The proportion of frame delay is less than expectations");
+            log.write(LogLevel::Debug, "Proportion of frame delay is less than expectation");
         }
         else
         {
-            log.write(LogLevel::Debug, "The proportion of frame delay is higher than expected");
+            log.write(LogLevel::Debug, "Proportion of frame delay exceeded expectation");
             if (nice <= right * 11 / 10) // 掉帧多了，卡顿
             {
-                log.write(LogLevel::Debug, "Exceeded Expectation Rating : 1");
+                log.write(LogLevel::Debug, "Exceeded expectation rating: 1");
                 cpu_controller.limit(1);
             }
             else if (nice <= right * 12 / 10)
             {
-                log.write(LogLevel::Debug, "Exceeded Expectation Rating : 2");
+                log.write(LogLevel::Debug, "Exceeded expectation rating: 2");
                 cpu_controller.limit(2);
             }
             else
             {
-                log.write(LogLevel::Debug, "Exceeded Expectation Rating : 3");
+                log.write(LogLevel::Debug, "Exceeded expectation rating: 3");
                 cpu_controller.limit(3);
             }
         }
