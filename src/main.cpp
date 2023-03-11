638e8d896d7d8d24138471104ccb2b2bae40577e
shadow3 <1394025007@qq.com>
2023-03-11 14:33:23 +0000

    disable.debug

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main()
 
     Log &log = Log::getLog("/storage/emulated/0/Android/FAS/FasLog.txt");
     log.setLevel(LogLevel::Info);
-    log.setLevel(LogLevel::Debug);
+    // log.setLevel(LogLevel::Debug);
     log.write(LogLevel::Info, "Log started");
 
     Cpufreq &cpu_controller = Cpufreq::getCpufreq();
