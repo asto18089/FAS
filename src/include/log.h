80a032664337389f215c91dcaf42e25fd7498e26
1394025007@qq.com <1394025007@qq.com>
2023-03-11 13:29:51 +0800

    modified:   src/include/log.h

--- src/include/log.h
+++ src/include/log.h
@@ -6,10 +6,28 @@
 
 #define DF_LOGLEVEL LogLevel::Debug
 #define DF_LOGFILE "/storage/emulated/0/Android/FAS/FasLog.txt"
+
+#if DF_LOGLEVEL == LogLevel::Debug
 #define DEBUG(x) Log::write(LogLevel::Debug, x)
 #define INFO(x) Log::write(LogLevel::Info, x)
 #define WARN(x) Log::write(LogLevel::Warning, x)
 #define ERROR(x) Log::write(LogLevel::Error, x)
+#elif DF_LOGLEVEL == LogLevel::Info
+#define DEBUG(x)
+#define INFO(x) Log::write(LogLevel::Info, x)
+#define WARN(x) Log::write(LogLevel::Warning, x)
+#define ERROR(x) Log::write(LogLevel::Error, x)
+#elif DF_LOGLEVEL == LogLevel::Warning
+#define DEBUG(x)
+#define INFO(x)
+#define WARN(x) Log::write(LogLevel::Warning, x)
+#define ERROR(x) Log::write(LogLevel::Error, x)
+#elif DF_LOGLEVEL == LogLevel::Error
+#define DEBUG(x)
+#define INFO(x)
+#define WARN(x)
+#define ERROR(x) Log::write(LogLevel::Error, x)
+
 
 enum class LogLevel
 {
